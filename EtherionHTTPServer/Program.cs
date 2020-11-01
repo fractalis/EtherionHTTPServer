a20d15d65cb2bedbd23766eebae0f910fc32ba68
Scott Rallya <srallya@gmail.com>
2020-11-01 08:46:02 -0500

    Create a Socket and Listen for an incoming Connection.

--- EtherionHTTPServer/Program.cs
+++ EtherionHTTPServer/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace EtherionHTTPServer
 {
+
     class Program
     {
+        const int MAX_CONNECTIONS = 5;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint = new IPEndPoint(System.Net.IPAddress.Parse("127.0.0.1"), 6789);
+
+            socket.Bind(endPoint);
+            socket.Listen(MAX_CONNECTIONS);
+
+
+            Socket connection = socket.Accept();
+
+            Console.ReadLine();
         }
     }
 }
